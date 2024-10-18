886ebde948e5f9a2c5f8ce5e929a23e79e32f846
Andrey Muravin <muravin@bk.ru>
2024-10-18 23:05:23 +0300

    Настройка пользователя

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+
+    const { user } = useUser();
     return ( 
-        <div>
-        Это главная страница
+        <div className="h-full flex flex-col
+        items-center justify-center space-y-4">
+        <Image 
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Пусто"
+        className="dark:hidden"
+        />
+        <Image 
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Пусто"
+        className="hidden dark:block"
+        />
+        <h2 className="text-lg font-bold">
+            Добро пожаловать, {user?.firstName}, в Murion!
+            </h2>
+            <Button className="">
+                <PlusCircle className="h-5 w-5 mr-2" />
+                Создать заметку
+            </Button>
+
+
         </div>
      );
 }
